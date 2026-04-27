8f7b708acae8dd891e07faa3108b93a52063b685
themontration <themontration@gmail.com>
2026-04-27 03:53:27 +0330

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,5 +1,3 @@
-export const config = { runtime: "edge" };
-
-export default function handler(req) {
-  return new Response("hello from vercel edge");
+export default function handler(req, res) {
+  res.status(200).send("hello");
 }
